6bdb00da86591c680c6b50958b95e123153ac70e
Tim Meusel <tim@bastelfreak.de>
2016-11-03 00:11:54 +0100

    rubocop: fix RSpec/ImplicitExpect

--- spec/classes/drbd_spec.rb
+++ spec/classes/drbd_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe 'drbd', type: :class do
-  it { should contain_class('drbd::service') }
-  it { should contain_package('drbd') }
-  it { should contain_exec('modprobe drbd') }
-  it { should contain_file('/drbd') }
-  it { should contain_file('/etc/drbd.conf') }
-  it { should contain_file('/etc/drbd.d').with_ensure('directory').with_purge(true) }
+  it { is_expected.to contain_class('drbd::service') }
+  it { is_expected.to contain_package('drbd') }
+  it { is_expected.to contain_exec('modprobe drbd') }
+  it { is_expected.to contain_file('/drbd') }
+  it { is_expected.to contain_file('/etc/drbd.conf') }
+  it { is_expected.to contain_file('/etc/drbd.d').with_ensure('directory').with_purge(true) }
   it do
-    should contain_file('/etc/drbd.d/global_common.conf').with_content %r{usage-count no;$}
-    should contain_file('/etc/drbd.d/global_common.conf').with_content %r{protocol C;$}
+    is_expected.to contain_file('/etc/drbd.d/global_common.conf').with_content %r{usage-count no;$}
+    is_expected.to contain_file('/etc/drbd.d/global_common.conf').with_content %r{protocol C;$}
   end
 end
