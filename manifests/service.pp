0f35cdecdfc5a9ea4deab57e3fb3db88c2f1023d
Slm0n87 <github@firebo.de>
2019-05-11 19:09:26 +0200

    Add variable for service_ensure
    - special condition for 'unmanaged'
      ensure has to be undef if puppet should not handle it (pacemaker does!)

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,13 @@
 class drbd::service {
+
+  if $drbd::service_ensure == 'unmanaged' {
+    $_ensure = undef
+  } else {
+    $_ensure = $drbd::service_ensure
+  }
+
   @service { 'drbd':
-    ensure  => running,
+    ensure  => $_ensure,
     enable  => $drbd::service_enable,
     require => Package['drbd'],
     restart => 'service drbd reload',
