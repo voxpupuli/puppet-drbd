870593d24bfe3bb36a50209ea8fe2ba7a5439eb6
Simon Stier <simon.stier@ionos.com>
2019-07-01 08:56:48 +0200

    add datatypes for new parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,9 +5,9 @@
 # by an example created by Rackspace's cloudbuilders
 #
 class drbd(
-  $service_enable = true,
-  $service_ensure = running,
-  $package_name = 'drbd8-utils',
+  $service_enable                                         = true,
+  Enum['running', 'stopped', 'unmanaged'] $service_ensure = running,
+  $package_name                                           = 'drbd8-utils',
 ) {
   include drbd::service
 
