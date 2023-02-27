a6c282c568181a88607c8f402f1e6c59f4bbcfdd
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2023-02-27 15:29:11 +0100

    add global and common options parameters

---
+++ spec/type_aliases/common_suboptions_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'Drbd::Common_suboptions' do
+  describe 'valid types' do
+    context 'with valid types' do
+      [
+        {},
+        { 'options' => [] },
+        { 'net' => %w[somevalue another] },
+      ].each do |value|
+        describe value.inspect do
+          it { is_expected.to allow_value(value) }
+        end
+      end
+    end
+  end
+
+  describe 'invalid types' do
+    context 'with garbage inputs' do
+      [
+        true,
+        nil,
+        { 'foo' => 'bar' },
+        'a string',
+      ].each do |value|
+        describe value.inspect do
+          it { is_expected.not_to allow_value(value) }
+        end
+      end
+    end
+  end
+end
